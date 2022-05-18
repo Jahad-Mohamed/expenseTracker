a348451e66fae7b8cdcd60c215bb0f688fac56a0
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-18 18:59:10 +0100

    expense files created, props passed

---
+++ src/conponents/Expenses/ExpenseDate.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./expenseDate.css";
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleDateString("en-UK", { month: "long" });
+  const day = props.date.toLocaleDateString("en-UK", { day: "numeric" });
+  const year = props.date.getFullYear();
+  return (
+    <div>
+      <div className="expense-date">
+        <div className="expense-date__day">{day}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
