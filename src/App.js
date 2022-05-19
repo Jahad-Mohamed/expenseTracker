c2fa1397a83edae840a6b81cd2193621a92f1ac5
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-19 10:48:43 +0100

    expenses list filter added, with condititonal statement

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import react, { useState } from "react";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Petrol",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Grocery Shopping",
+    amount: 91.54,
+    date: new Date(2022, 3, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: "e3",
+    id: "e2",
+    title: "Rent",
+    amount: 1294.12,
+    date: new Date(2022, 1, 14),
+  },
+  { id: "e3", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e4",
     title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: "e4",
+    id: "e5",
     title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e6",
+    title: "New TV ",
+    amount: 799.9,
+    date: new Date(2020, 1, 22),
   },
 ];
 
